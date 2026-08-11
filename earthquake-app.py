28a2e036220151370e19a193ef75335d4580b4e0
Lakshya190104 <lakshya.19sri@gmail.com>
2026-08-11 19:08:21 +0530

    Add addition feature to earthquake app

--- earthquake-app.py
+++ earthquake-app.py
@@ -96,3 +96,8 @@ st.sidebar.info(
     Use the slider to filter earthquakes by magnitude. The times are displayed in both UTC and local time.
     """
 )
+st.title("addition")
+num1= st.number_input("enter 1st no.", value=0.0, step=1.0)
+num2= st.number_input("enter  the second number", value=0.0, step=1.0)
+total= num1+ num2
+st.success(f"the sum of **{num1}** and **{num2}** is **{total}**)
